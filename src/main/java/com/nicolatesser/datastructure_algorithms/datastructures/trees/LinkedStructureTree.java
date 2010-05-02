a0aca6b6694c4c8c01809094e7cee45a5d428506
Your Name <ghiottolino+github@gmal.com>
2010-05-02 12:03:17 +0200

    added string matching algorithms

---
+++ src/main/java/com/nicolatesser/datastructure_algorithms/datastructures/trees/LinkedStructureTree.java
@@ -0,0 +1,55 @@
+package com.nicolatesser.datastructure_algorithms.datastructures.trees;
+
+import java.util.List;
+
+public class LinkedStructureTree implements Tree {
+
+	@Override
+	public List<Position> children(Position p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isExternal(Position p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInternal(Position p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRoot(Position p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Position parent(Position p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position root() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setChildren(List<Position> children) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/com/nicolatesser/datastructure_algorithms/datastructures/trees/Position.java
@@ -0,0 +1,5 @@
+package com.nicolatesser.datastructure_algorithms.datastructures.trees;
+
+public interface Position {
+
+}
