342d643c66d47019f101dd2d4c88fd7156698835
Your Name <ghiottolino+github@gmal.com>
2010-05-23 20:23:41 +0200

    added bit arithmetic algorithms templates

---
+++ src/main/java/com/nicolatesser/datastructure_algorithms/bit_arithmetic/BitArithmeticAlgorithms.java
@@ -0,0 +1,30 @@
+package com.nicolatesser.datastructure_algorithms.bit_arithmetic;
+
+public class BitArithmeticAlgorithms {
+
+	
+	public String sum(String a, String b)
+	{
+		//TODO : implement
+		return null;
+	}
+	
+	public String subtract(String a, String b)
+	{
+		//TODO : implement
+		return null;
+	}
+	
+	public String multiplyPowerOf2(String a, String b)
+	{
+		//TODO : implement
+		return null;
+	}
+	
+	public String multiply(String a, String b)
+	{
+		//TODO : implement
+		return null;
+	}
+	
+}
