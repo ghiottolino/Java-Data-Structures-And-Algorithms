eaaf4509f0ec41b0201f678ca59f837af62872cf
Your Name <ghiottolino+github@gmal.com>
2010-05-24 13:40:15 +0200

    added tree implementation

---
+++ src/main/java/com/nicolatesser/datastructure_algorithms/datastructures/tree/Position.java
@@ -0,0 +1,10 @@
+package com.nicolatesser.datastructure_algorithms.datastructures.tree;
+
+import java.util.List;
+
+import com.nicolatesser.datastructure_algorithms.datastructures.tree.linked_structure.Node;
+
+public interface Position {
+	
+	
+}
